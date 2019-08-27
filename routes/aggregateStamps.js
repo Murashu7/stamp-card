a100d67d1440f01b9bf536e67bf924a89fe9a0ca
Shuichi Murakami <murashu7@gmail.com>
2019-08-27 15:17:49 +0000

    新規フォルダとファイル追加

---
+++ routes/aggregateStamps.js
@@ -0,0 +1,36 @@
+'use strict'
+const Stamp = require('../models/stamp');
+const moment = require('moment-timezone');
+const loader = require('../models/sequelize-loader');
+const sequelize = loader.database;
+
+const aggregateStamps = function(objective, today) {
+  const createdAt = moment(objective.createdAt); // 作成日
+  const dueDay = moment(objective.dueDay); // 期限日
+  const frequency = objective.frequency;
+  const diffWeeks_t = today.diff(createdAt, 'weeks') + 1; // 作成日から今日までの週数
+  const diffWeeks_d = dueDay.diff(createdAt, 'weeks') + 1; // 作成日から期限までの週数
+  const goalTimes_t = (frequency * diffWeeks_t); // 作成日から今日までの目標回数
+  const goalTimes_d = (frequency * diffWeeks_d); // 作成日から期限までの目標回数
+
+  return Stamp.findOne({
+    where: { 
+      objectiveId: objective.objectiveId,
+      stampStatus: true
+    },
+    attributes: [
+      [sequelize.fn('count', sequelize.col('stampStatus')), 'countStatus']
+    ]
+  }).then((result) => {
+    return new Promise((resolve) => {
+       const implTimes = result.dataValues['countStatus'];
+       objective.freqAchvRate_p = Math.round((implTimes / goalTimes_t) * 100); // 今日までの達成率(%)
+       objective.freqAchvRate_f = `( ${implTimes} ／ ${goalTimes_t} )`; // 今日までの達成率(分数)
+       objective.objAchvRate_p = Math.round((implTimes / goalTimes_d) * 100); // 期限日まで達成率(%)
+       objective.objAchvRate_f = `( ${implTimes} ／ ${goalTimes_d} )`; // 期限日までの達成率(分数)
+       resolve(objective);
+    });
+  })
+}
+
+module.exports = aggregateStamps;
---
+++ utils/colorLog.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const colorLog = {
+  blak:'\u001b[30m',
+  red:'\u001b[31m',
+  green:'\u001b[32m',
+  yellow:'\u001b[33m',
+  blue:'\u001b[34m',
+  magenta:'\u001b[35m',
+  cyan:'\u001b[36m',
+  white:'\u001b[37m',
+  reset:'\u001b[0m',
+  color: function(color, value) {
+    const c = eval('this.' + color);
+    console.log(`${c}${value}${this.reset}`);
+  }
+}
+
+module.exports = colorLog;
